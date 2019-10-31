42df841e5c24dd3bbf29e4c901376c2a7ea1f47d
wangdazhi <wangdazhi @tsa.cn>
2019-10-31 14:21:15 +0800

    分环境打包

--- src/main.js
+++ src/main.js
@@ -12,8 +12,8 @@ import ElementUI from 'element-ui';
 
 Vue.prototype.Clipboard = Clipboard;
 Vue.prototype.$request = request;
-Vue.prototype.$http = 'http://39.107.66.190:8080/';
-Vue.prototype.$baseURL = 'http://39.107.66.190:8080' //'http://192.168.50.144:8080/'
+Vue.prototype.$http = process.env.VUE_APP_BASEURL;
+Vue.prototype.$baseURL = process.env.VUE_APP_APIURL + '/' //'http://192.168.50.144:8080/'
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
